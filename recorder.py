9ae4684739de79445acadb519f7f8c4461691bd8
Adam Washington <adam.washington@stfc.ac.uk>
2017-03-07 17:12:39 +0000

    Save to file

--- recorder.py
+++ recorder.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python
 
 from pynput import mouse, keyboard
+from sys import argv
 
-def on_click(x, y, button, pressed):
-    print("{0} at {1}".format(
-        "Pressed" if pressed else "Released",
-        (x, y)))
+def on_clicker(handle):
+    def on_click(x, y, button, pressed):
+        handle.write("{0} at {1}\n".format(
+            "Pressed" if pressed else "Released",
+            (x, y)))
+    return on_click
 
-def on_scroll(x, y, dx, dy):
-    print("Scrolled at {0} by {1}".format(
-        (x, y),
-        (dx, dy)))
+def on_scroller(handle):
+    def on_scroll(x, y, dx, dy):
+        handle.write("Scrolled at {0} by {1}\n".format(
+            (x, y),
+            (dx, dy)))
+    return on_scroll
 
-with mouse.Listener(
-        on_click=on_click,
-        on_scroll=on_scroll) as listener:
-
+def on_presser(handle):
     def on_press(key):
-        print('key {0} pressed'.format(key))
+        handle.write('key {0} pressed\n'.format(key))
+    return on_press
 
+def on_releaser(handle, listener):
     def on_release(key):
-        print("key {0} released".format(key))
+        handle.write("key {0} released\n".format(key))
         if key == keyboard.Key.esc:
             listener.stop()
             return False
+    return on_release
 
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as klistener:
-        klistener.join()
-        listener.join()
+print(argv)
+with open(argv[1], "w") as outfile:
+    with mouse.Listener(
+            on_click=on_clicker(outfile),
+            on_scroll=on_scroller(outfile)) as listener:
+        with keyboard.Listener(
+                on_press=on_presser(outfile),
+                on_release=on_releaser(outfile, listener)) as klistener:
+            klistener.join()
+            listener.join()
