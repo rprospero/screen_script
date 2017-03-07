999b1c2c2cf22b39567d8668b7967315cba31f1c
Adam Washington <adam.washington@stfc.ac.uk>
2017-03-07 16:57:46 +0000

    Merge in keyboard control

--- recorder.py
+++ recorder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from pynput import mouse
+from pynput import mouse, keyboard
 
 def on_click(x, y, button, pressed):
     print("{0} at {1}".format(
@@ -15,4 +15,18 @@ def on_scroll(x, y, dx, dy):
 with mouse.Listener(
         on_click=on_click,
         on_scroll=on_scroll) as listener:
-    listener.join()
+
+    def on_press(key):
+        print('key {0} pressed'.format(key))
+
+    def on_release(key):
+        print("key {0} released".format(key))
+        if key == keyboard.Key.esc:
+            listener.stop()
+            return False
+
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as klistener:
+        klistener.join()
+        listener.join()
